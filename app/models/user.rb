d39338133923faa96e9c40fc328f2daec042ff4e
rubytaiki <taikimalay@gmail.com>
2021-07-20 18:53:51 +0900

    新規登録機能エラーの修正

--- app/models/user.rb
+++ app/models/user.rb
@@ -8,7 +8,6 @@ class User < ApplicationRecord
   validates :profile, presence: true
   validates :occupation, presence: true
   validates :position, presence: true
-  validates :image, presence: true
   
   has_many :prototypes
   has_many :comments
