f931abd2e8c6783a2d8742802a5ea299972f6b30
rubytaiki <taikimalay@gmail.com>
2021-07-20 17:21:59 +0900

    デプロイのための設定

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -44,15 +44,8 @@ class PrototypesController < ApplicationController
     redirect_to root_path
   end
 
-
-
-
   private
   def prototype_params
     params.require(:prototype).permit(:title, :catch_copy, :concept, :image).merge(user_id: current_user.id)
   end
-
-  
-  
-
 end
